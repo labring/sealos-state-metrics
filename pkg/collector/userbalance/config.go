0ac063137a073c45b33bc2b85ae0e82c28e55d85
Bearix <2669184984@qq.com>
2026-03-20 11:25:11 +0800

    fix userbalance logic. (#53)

--- pkg/collector/userbalance/config.go
+++ pkg/collector/userbalance/config.go
@@ -11,12 +11,13 @@ type DatabaseConfig struct {
 
 // UserConfig holds configuration for a single user/account
 type UserConfig struct {
-	Region string `yaml:"region" json:"region"` // Cloud service region
-	UUID   string `yaml:"uuid"   json:"uuid"`   // User unique identifier
-	UID    string `yaml:"uid"    json:"uid"`    // User ID
-	Owner  string `yaml:"owner"  json:"owner"`  // Account owner
-	Type   string `yaml:"type"   json:"type"`   // User type
-	Level  string `yaml:"level"  json:"level"`  // User level
+	Region string  `yaml:"region" json:"region"` // Cloud service region
+	UUID   string  `yaml:"uuid"   json:"uuid"`   // User unique identifier
+	UID    string  `yaml:"uid"    json:"uid"`    // User ID
+	Owner  string  `yaml:"owner"  json:"owner"`  // Account owner
+	Type   string  `yaml:"type"   json:"type"`   // User type
+	Level  string  `yaml:"level"  json:"level"`  // User level
+	Quota  float64 `yaml:"quota"  json:"quota"`  // User Quota
 }
 
 // Config contains configuration for the UserBalance collector
